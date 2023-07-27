9243e82a391061e0c5d4a4757fc52dc8d4ad6b55
504875043@qq.com <504875043@qq.com>
2023-07-27 13:23:49 +0800

    =>2023-07-27 13:23:49

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+###
+ # @Author: 程英明
+ # @Date: 2023-07-27 13:22:32
+ # @LastEditTime: 2023-07-27 13:23:41
+ # @LastEditors: 程英明
+ # @Description: 
+ # @FilePath: \web_php6_server\install.sh
+ # QQ:504875043@qq.com
+### 
+composer require topthink/think-multi-app
+composer require topthink/think-filesystem
+composer require findnr/cym-aliyun
+composer require findnr/cym-phptools
+composer require ramsey/uuid
+composer require gregwar/captcha
