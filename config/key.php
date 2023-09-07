2ec1cfc33426f3ebf02291b165535c718847f60d
cym504875043 <504875043@qq.com>
2023-09-07 10:16:41 +0800

    =>2023-09-07 10:16:41

---
+++ config/key.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * @Author: 程英明
+ * @Date: 2021-04-23 15:31:19
+ * @LastEditTime: 2023-09-07 10:16:31
+ * @LastEditors: 程英明
+ * @Description: 
+ * @FilePath: \thinkphp_api\config\key.php
+ * QQ:504875043@qq.com
+ */
+
+return [
+    'api_key' => 'gdfgfdg',
+    'token_key' => 'gfdgfdgfdg',
+    'token_time' => 2592000,
+    'qrcode_key' => 'gfdsgfdsgfdsg',
+    'admin_token_key' => 'gfdsgfdsgfdgfd',
+    'user_token_key' => 'ggfdsgfdsgfa',
+    'unit_token_key' => 'gfdsgfdsgfdsg',
+    'person_token_key' => 'gdfsgfdsgfdsg',
+    'man_token_key' => 'gfdsgfdsgdsgfds',
+];
