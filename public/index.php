3796ef43d5516d2ed4494ca7da49264333e8df4a
root <root@localhost.localdomain>
2023-09-07 09:47:57 +0800

    =>2023-09-07 09:47:57

--- public/index.php
+++ public/index.php
@@ -1,18 +1,24 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006-2019 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
+/*
+ * @Author: 程英明
+ * @Date: 2021-05-17 09:46:42
+ * @LastEditTime: 2021-11-26 11:35:11
+ * @LastEditors: 程英明
+ * @Description: 
+ * @FilePath: \api\public\index.php
+ * QQ:504875043@qq.com
+ */
 
-// [ 应用入口文件 ]
 namespace think;
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
+header('Access-Control-Allow-Methods: GET, POST, PUT,DELETE,OPTIONS,PATCH');
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    return;
+};
 require __DIR__ . '/../vendor/autoload.php';
+
 
 // 执行HTTP应用并响应
 $http = (new App())->http;
