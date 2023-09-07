b7f28315968898624d9c573a2067337c5006c3a5
cym504875043 <504875043@qq.com>
2023-09-07 09:55:06 +0800

    =>2023-09-07 09:55:06

--- install.sh
+++ install.sh
@@ -2,12 +2,13 @@
 ###
  # @Author: 程英明
  # @Date: 2023-07-27 13:22:32
- # @LastEditTime: 2023-07-27 13:23:41
+ # @LastEditTime: 2023-09-07 09:54:58
  # @LastEditors: 程英明
  # @Description: 
- # @FilePath: \web_php6_server\install.sh
+ # @FilePath: \thinkphp_api\install.sh
  # QQ:504875043@qq.com
 ### 
+composer install
 composer require topthink/think-multi-app
 composer require topthink/think-filesystem
 composer require findnr/cym-aliyun
