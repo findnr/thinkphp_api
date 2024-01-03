2de3fe5da5aa85a9d874a2f9989a9f7061ce5d2a
cym504875043 <504875043@qq.com>
2024-01-03 16:23:02 +0800

    =>2024-01-03 16:23:02

--- app/common/install/bases/AdminNav.php
+++ app/common/install/bases/AdminNav.php
@@ -2,7 +2,7 @@
 /*
  * @Author: 程英明
  * @Date: 2023-04-11 15:04:37
- * @LastEditTime: 2023-05-09 15:01:38
+ * @LastEditTime: 2024-01-03 16:19:53
  * @LastEditors: 程英明
  * @Description: 
  * @FilePath: \api\app\common\install\bases\AdminNav.php
@@ -40,27 +40,31 @@ class AdminNav extends Base
           ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci COMMENT='后台导航表信息'
         str;
             $sql[] = <<<str
-        INSERT INTO `admin_nav` (`id`, `name`, `index_url`, `admin_url`, `icon`, `nav_type_id`, `action_id`, `sort`, `remark`, `parent_id`, `status`) VALUES
-        (1, '系统首页', '/admin/home/home', '/admin/home/home', 'house', 1, '', 0, '', 0, 0),
-        (2, '系统设置', '', '', 'tools', 1, '', 9, '', 0, 0),
-        (3, '后台导航', '/admin/system/nav', '/admin/nav/nav', '', 1, '1,2,3,4', 100, '', 2, 0),
-        (4, '导航类型', '/admin/system/navtype', '/admin/system/navtype', '', 1, '1,2,3,4', 100, '', 2, 0),
-        (5, '操作动作', '/admin/system/action', '/admin/system/action', '', 1, '1,2,3,4', 100, '', 2, 0),
-        (6, '个人中心', '', '', 'user-filled', 1, '', 10, '', 0, 0),
-        (7, '个人信息', '/admin/user/info', '/admin/user/info', '', 1, '1,2,3,4', 100, '', 6, 0),
-        (8, '修改密码', '/admin/user/password', '/admin/user/password', '', 1, '1,2,3,4', 100, '', 6, 0),
-        (9, '退出系统', '/admin/user/loginout', '/admin/user/loginout', '', 1, '', 100, '', 6, 0),
-        (10, '管理员管理', '', '', 'avatar', 1, '0', 8, '', 0, 0),
-        (11, '添加管理员', '/admin/mangeruser/user', '/admin/mangerUser/user', '', 1, '1,2,3,4', 100, '', 10, 0),
-        (12, '管理员权限管理', '/admin/mangeruser/auth', '/admin/mangerUser/auth', '', 1, '1,2,3,4', 100, '', 10, 0),
-        (13, '组管理', '/admin/mangeruser/group', '/admin/mangerUser/group', '', 1, '1,2,3,4', 100, '', 10, 0),
-        (14, '组权限管理', '/admin/mangeruser/groupauth', '/admin/mangerUser/groupAuth', '', 1, '1,2,3,4', 100, '', 10, 0)
+            INSERT INTO `admin_nav` (`id`, `name`, `index_url`, `admin_url`, `icon`, `nav_type_id`, `action_id`, `sort`, `remark`, `parent_id`, `status`) VALUES
+            (1, '系统首页', '/admin/home/home', '/admin/home/home', 'house', 1, '', 0, '', 0, 0),
+            (2, '系统设置', '', '', 'tools', 1, '', 9, '', 0, 0),
+            (3, '后台导航', '/admin/system/nav', '/admin/nav/nav', '', 1, '1,2,3,4', 100, '', 2, 0),
+            (4, '导航类型', '/admin/system/navtype', '/admin/system/navtype', '', 1, '1,2,3,4', 100, '', 2, 0),
+            (5, '操作动作', '/admin/system/action', '/admin/system/action', '', 1, '1,2,3,4', 100, '', 2, 0),
+            (6, '个人中心', '', '', 'user-filled', 1, '', 10, '', 0, 0),
+            (7, '个人信息', '/admin/user/info', '/admin/user/info', '', 1, '1,2,3,4', 100, '', 6, 0),
+            (8, '修改密码', '/admin/user/password', '/admin/user/password', '', 1, '1,2,3,4', 100, '', 6, 0),
+            (9, '退出系统', '/admin/user/loginout', '/admin/user/loginout', '', 1, '', 100, '', 6, 0),
+            (10, '管理员管理', '', '', 'avatar', 1, '0', 8, '', 0, 0),
+            (11, '添加管理员', '/admin/mangeruser/user', '/admin/mangerUser/user', '', 1, '1,2,3,4', 100, '', 10, 0),
+            (12, '管理员权限管理', '/admin/mangeruser/auth', '/admin/mangerUser/auth', '', 1, '1,2,3,4', 100, '', 10, 0),
+            (13, '组管理', '/admin/mangeruser/group', '/admin/mangerUser/group', '', 1, '1,2,3,4', 100, '', 10, 0),
+            (14, '组权限管理', '/admin/mangeruser/groupauth', '/admin/mangerUser/groupAuth', '', 1, '1,2,3,4', 100, '', 10, 0),
+            (15, '文件管理', '', '', 'Folder', 1, '', 0, '', 0, 0),
+            (16, '文件上传', '/admin/upload/list', '/admin/upload/list', '', 1, '1,2,3,4', 0, '', 15, 0),
+            (17, '文件分类管理', '/admin/upload/type', '/admin/upload/type', '', 1, '1,2,3,4', 0, '', 15, 0),
+            (18, '系统配制', '/admin/system/config', '/admin/system/config', '', 1, '1,2,3,4', 100, '', 2, 0);
         str;
             $sql[] = <<<str
         ALTER TABLE `admin_nav` ADD PRIMARY KEY (`id`)
         str;
             $sql[] = <<<str
-        ALTER TABLE `admin_nav` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增ID', AUTO_INCREMENT=15
+        ALTER TABLE `admin_nav` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增ID', AUTO_INCREMENT=19
         str;
         }
 
